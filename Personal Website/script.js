45cfe4e3a226ed402f645f5bf5149fdbca5c854e
silvesterhtet <zinnyilinhtet@gmail.com>
2024-10-21 15:59:18 -0700

    updated website

--- Personal Website/script.js
+++ Personal Website/script.js
@@ -21,3 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     type(); // Start typing effect
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active'); // Toggle active class on hamburger
+    });
+});
