8c21fbddb075e73d134d98515eea74879721e162
silvesterhtet <zinnyilinhtet@gmail.com>
2024-10-27 16:01:58 -0700

    Vitamin 4 done

--- Vitamin 4/my-app/src/components/Navbar/Navbar.js
+++ Vitamin 4/my-app/src/components/Navbar/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'; //import React
-import { BrowserRouter as Link } from 'react-router-dom'; // Import Link to navigate between pages
-import './Navbar.css' // Import the CSS file for styling
-import Home from '../../pages/Home/Home'
-import NotHome from '../../pages/NotHome/NotHome'
+import {  Link } from 'react-router-dom'; // Import Link to navigate between pages
+import './Navbar.css';// Import the CSS file for styling
+import Home from '../../pages/Home/Home';
+import NotHome from '../../pages/NotHome/NotHome';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-        <ul>
+        <div className="navbar-brand">
+            <Link to="/">My Website</Link>
+        </div>
+        <ul className="navbar-links">
             <li>
             <Link to="/home">Home</Link>
             </li>
@@ -17,7 +20,10 @@ const Navbar = () => {
         </ul>
         </nav>
     );
+     // fill in the rest here!
+     // add Home and NotHome, including links (after routing established)
   };
   
   // Always export your components so they can be imported in other files
   export default Navbar;
+  
--- Vitamin 4/my-app/src/pages/Home/Home.js
+++ Vitamin 4/my-app/src/pages/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
-};
+    return (
+        <div className="nothome">
+          <h1>Home Page</h1>
+        </div>
+      );
+    };
 
 export default Home;
