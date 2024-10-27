4c3d5ffea85c66da56b0f6a98f88355110eeefd9
silvesterhtet <zinnyilinhtet@gmail.com>
2024-10-27 12:58:06 -0700

    Vitamin 4 IP

---
+++ Vitamin 4/my-app/src/pages/NotHome/NotHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotHome = () => {
+  return (
+    <div>
+      <h1>Not Home Page</h1>
+      <p>Welcome to the NotHome page!</p>
+    </div>
+  );
+};
+
+export default NotHome;
